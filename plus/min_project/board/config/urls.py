ad838dfa43f811bbe08b221c6c018bbc7941f6f5
Jamikidu <kangjh1994@gmail.com>
2023-06-26 20:34:12 +0900

    1

---
+++ plus/min_project/board/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from board import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # http://127.0.0.1:8000
+    path('', views.index),
+    path('main/', views.main),
+    path('board/', include('board.urls')),
+    path('common2/', include('common2.urls')),
+]
